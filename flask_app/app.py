e9a6419b78b25f307a1e4e84b16c35e7c9971e3b
Pratish Barthwal <pratishbarthwal@gmail.com>
2023-07-26 01:38:44 +0530

    made some changes

--- flask_app/app.py
+++ flask_app/app.py
@@ -32,4 +32,4 @@ def handle_post_request():
         return jsonify({"message": "Data received successfully!", "prediction": int(prediction[0])})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
